1993ea561c366bb51d1d06dca5b24935f8a8c47e
tdr <tdr.evolution@gmail.com>
2023-01-31 10:07:17 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using WebProblemDetails.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebProblemDetails.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private List<Product> _produts;
+
+        public ProductController()
+        {
+            _produts = new List<Product>() {
+                new Product() { Id = 1, Name = "Product 1", Category = "Exemple"},
+                new Product() { Id = 2, Name = "Product 2", Category = "Exemple"},
+                new Product() { Id = 3, Name = "Product 3", Category = "Exemple"}
+            };
+        }
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _produts;
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = new Product();
+            IEnumerable<Product> products = _produts.Where(p => p.Id == id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product value)
+        {
+            if (!ModelState.IsValid) {
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(Get), new { id = value.Id }, value);
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (!_produts.Exists(p => p.Id == id)) {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!_produts.Exists(p => p.Id == id))
+            {
+                return NotFound();
+            }
+
+            return Ok();
+
+        }
+    }
+}
---
+++ WebApplication2/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProblemDetails.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo {PropertyName} precisa ser fornecido")]
+        public string Name { get; set; } 
+        public string Description { get; set; }
+        public string Category { get; set; }    
+
+    }
+}
